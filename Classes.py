abb60a920a859cf572280d2d53e9a985691b5f55
Zack <zackripalda@gmail.com>
2019-12-13 19:18:00 -0700

    Added a few pokemon classes, modified gender to include ratio

--- Classes.py
+++ Classes.py
@@ -65,7 +65,7 @@ class Move:
         # determines if the move has no effect
         if (self.type in element_no_effect) and (op_pokemon.type in element_no_effect[self.type]):
             effectiveness = 0
-            return stab, effectiveness
+            return effectiveness
         # checking for STAB (same-type attack bonus
         elif self.type in pokemon.elements:
             stab = True
@@ -73,14 +73,15 @@ class Move:
         for element in element_att_strengths[op_pokemon.type]:
             if self.type == element:
                 effectiveness *= 2
-        # chicking if type is "not that effective"
+        # checking if type is "not that effective"
         for element in element_def_weaknesses[op_pokemon.type]:
             if self.type == element:
                 effectiveness /= 2
-        if stab == True:
+        if stab:
             effectiveness *= 1.5
         return effectiveness
 
+    #will return weather or not a move hits
     def accuracy(self, pokemon, op_pokemon):
         pass
 
@@ -91,7 +92,7 @@ class Trainer:
 
 
 class Pokemon:
-    # xp_yield = (wild/owned * traded * base xp * feinted level)/7 * number in battle not feinted
+    # xp_yield = (wild/owned * traded * base xp * feinted level)/7 * number in battle not feinted /not implemented
 
     # initializes pokemon stats
     def __init__(self, name, elements, pokemon, gender, base_stats, evolutions=None,
@@ -103,7 +104,6 @@ class Pokemon:
         self.base_stats = base_stats
         self.EV = ev
         self.IV = {stat: random.randint(0, 15) for stat in self.base_stats.keys() if stat != "HP"}
-        hp_iv = []
         hp_binary = ''
         for item in self.IV.values():
             hp_binary += str(bin(item)[-1])
@@ -124,7 +124,6 @@ class Pokemon:
         self.gender = gender
         self.moves = []
         self.evolutions = evolutions
-        print(self.gender)
 
     # prints a description of the pokemon
     def __repr__(self):
@@ -142,12 +141,12 @@ class Pokemon:
                                                                                           self.moves)
 
     # calculates EV gain of a pokemon when called (usually after a battle)
-    def ev_gain(self, op_pokemon):
+    def Ev_gain(self, op_pokemon):
         for stat in op_pokemon.base_stats:
             self.EV[stat] += op_pokemon.base_stats[stat]
 
     # changes pokemon level, unfinished/needs to check possible moves to learn
-    def level_up(self):
+    def Level_up(self):
         if self.lvl < 100:
             self.lvl += 1
             self.hp = math.floor((math.floor(((self.base_stats["HP"] + self.IV["HP"]) * 2 + math.floor(
@@ -155,32 +154,34 @@ class Pokemon:
             self.battle_stats = {stat: int(math.floor((((self.base_stats[stat] + self.IV[stat]) * 2 + math.floor(
                 math.ceil(math.sqrt(self.EV[stat])) / 4)) * self.lvl) / 100) + 5) for stat in self.base_stats.keys() if
                                  stat != "HP"}
+            self.xp_to_next = (self.lvl + 1) ** 3
             # weather or not the pokemon evolves
             if self.evolutions is not None:
                 if self.lvl == self.evolutions[0][0]:
                     self.pokemon = self.evolutions[0][1]
 
     # adds earned xp to pokemons xp and calls the level_up function if they earned enough to be at the next level
-    def xp_gain(self, xp_earned):
+    def Xp_gain(self, xp_earned):
         self.xp += xp_earned
         if self.xp == self.xp_to_next:
-            self.level_up()
+            self.Level_up()
 
 
-# basic function for randomizing gender , possible refinement to reflect actual pokemon gender ratios
-def get_gender():
+# basic function for randomizing gender , takes r, ratio of male to females. higher ratio means more females
+# r should always be a number between 0 and 1, if no ratio is defined then it is always 50/50
+def get_gender(r = .5):
     g = random.randint(0, 100)
-    if g > 50:
+    if g >= r*100:
         return "Male"
     else:
         return "Female"
 
 
 # new code to change the games difficulty curve, randomly generated pokemon will be at a level equal to the average level
-# of the trainers current roster.
+# of the trainers current roster. default level is between 1 and 5
 def pokemon_level(team=None):
     if team is None:
-        return 5
+        return random.randint(1,5)
     else:
         level = 0
         for pokemon in team:
@@ -189,7 +190,7 @@ def pokemon_level(team=None):
         return level
 
 
-# example of defining each species of pokemons specific traits
+# example of defining each species of pokemon's specific traits
 # defining each pokemon's base characteristics, to do: add all pokemon's learnable moves.
 class Bulbasaur(Pokemon):
     def __init__(self, name="Bulbasaur"):
@@ -199,14 +200,72 @@ class Bulbasaur(Pokemon):
         pokemon_n = 1
 
         super().__init__(name, elements, pokemon_n, gender, base_stats)
+class Ivysaur(Pokemon):
 
-    # {"HP": 45, "ATTACK": 49, "DEFENSE": 45, "SPEED": 45, "SPECIAL": 65} ivysaur = Pokemon("Ivysaur", ["GRASS,
-    # POISON"], 2, get_gender(), {"HP":60,"ATTACK":62,"DEFENSE":63,"SPEED":60,"SPECIAL":80}, evolutions={"Venusaur":[
-    # 32,3]}) venusaur = Pokemon("Venusaur", ["GRASS", "POISON"], 3, get_gender(), {"HP":80,"ATTACK":82,"DEFENSE":83,
-    # "SPEED":80,"SPECIAL":100}) charmander = Pokemon("Charmander", ["FIRE"], 4, get_gender(),{"HP":39,"ATTACK":52,
-    # "DEFENSE":43,"SPEED":65,"SPECIAL":50}, evolutions={"Charmeleon":[16,4]}) charmeleon = Pokemon("Venusaur",
-    # ["FIRE"], 4, get_gender(), {"HP":58,"ATTACK":64,"DEFENSE":58,"SPEED":80,"SPECIAL":65})
+    def __init__(self, name= "Ivysaur"):
+        elements = ["GRASS, POISON"],
+        pokemon_n = 2
+        gender = get_gender()
+        base_stats = {"HP":60,"ATTACK":62,"DEFENSE":63,"SPEED":60,"SPECIAL":80}
+        evolutions={"Venusaur":[32,3]}
 
+        super().__init__(name, elements, pokemon_n, gender, base_stats)
+
+class Venusaur(Pokemon):
+    def __init__(self, name="Venusaur"):
+        elements = ["GRASS", "POISON"],
+        pokemon_n = 3
+        gender = get_gender()
+        base_stats = {"HP":80,"ATTACK":82,"DEFENSE":83, "SPEED":80,"SPECIAL":100}
+
+        super().__init__(name, elements, pokemon_n, gender, base_stats)
+
+class Charmander(Pokemon):
+    def __init__(self, name = "Charmander"):
+        elements = ["FIRE"]
+        pokemon_n = 4
+        gender = get_gender()
+        base_stats = {"HP":39,"ATTACK":52, "DEFENSE":43,"SPEED":65,"SPECIAL":50}
+        evolutions={"Charmeleon":[16,4]})
+
+        super().__init__(name, elements, pokemon_n, gender, base_stats, evolutions)
+
+class Charmeleon(Pokemon):
+    def __init__(self, name = "Charmeleon"):
+        elements =  ["FIRE"]
+        pokemon_n = 5
+        gender = get_gender()
+        base_stats = {"HP":58,"ATTACK":64,"DEFENSE":58,"SPEED":80,"SPECIAL":65}
+
+        super().__init__(name, elements, pokemon_n, gender, base_stats)
+
+class Charizard(Pokemon):
+    def __init__(self, name = "Charizard"):
+        elements = ["FIRE"]
+        pokemon_n = 6
+        gender = get_gender()
+        base_stats = {"HP": 78,"ATTACK": 84,"DEFENSE": 78, "SPEED": 100, "SPECIAL": 86}
+
+        super().__init__(name, elements, pokemon_n, gender, base_stats)
+
+class Squirtle(Pokemon):
+    def __init__(self, name = "Squirtle"):
+        elements = ["WATER"]
+        pokemon_n = 7
+        gender = get_gender()
+        base_stats = {"HP": ,"ATTACK": ,"DEFENSE": , "SPEED": , "SPECIAL":}
+
+        super().__init__(name, elements, pokemon_n, gender, base_stats)
+# empty pokemon creation class
+"""class (Pokemon):
+    def __init__(self, name = ""):
+        elements = []
+        pokemon_n =
+        gender = get_gender()
+        base_stats = {"HP": ,"ATTACK": ,"DEFENSE": , "SPEED": , "SPECIAL"}
+
+        super().__init__(name, elements, pokemon_n, gender, base_stats)
+"""
     #  i did this for some reason, and i forgot, could be useful later
 """pokemon = {
         0:None,
