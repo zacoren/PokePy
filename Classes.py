79b0f79753903b013ca73a43db832e98f31e00ef
Zack <zackripalda@gmail.com>
2019-12-14 16:34:29 -0700

    Added feint and hp functions to pokemon class, fixed a few errors

--- Classes.py
+++ Classes.py
@@ -44,7 +44,7 @@ element_no_effect = {
 }
 
 
-# returns a function that modifies pokemon behavior based on move statuses
+# returns a function that modifies pokemon behavior based on move statuses / unfinished
 def status_effects(status):
     pass
 
@@ -52,11 +52,11 @@ def status_effects(status):
 # defines moves and their abilities HORRIBLY UNFINISHED
 class Move:
 
-    def __init__(self, type, damage, accuracy=100, status_effects=None):
+    def __init__(self, type, damage, accuracy=100, status_effect=None):
         self.type = type
         self.damage = damage
         self.accuracy = accuracy
-        self.status_effects = status_effects
+        self.status_effects = status_effect
 
     # returns weather STAB is available, and returns super/not-very effectiveness
     def get_effective(self, pokemon, op_pokemon):
@@ -81,14 +81,18 @@ class Move:
             effectiveness *= 1.5
         return effectiveness
 
-    #will return weather or not a move hits
+    # will return weather or not a move hits
     def accuracy(self, pokemon, op_pokemon):
         pass
 
 
-# Defines trainer class, will be used to super() player class later.
+# Defines trainer class, will be used to super() player class later, and possibly specific types of trainers /unfinished
 class Trainer:
-    pass
+    def __init__(self, name, money=None, items=None, pokemon=None):
+        self.name = name
+        self.pokemon = pokemon
+        self.money = money
+        self.item = items
 
 
 class Pokemon:
@@ -96,14 +100,19 @@ class Pokemon:
 
     # initializes pokemon stats
     def __init__(self, name, elements, pokemon, gender, base_stats, evolutions=None,
-                 ev={"HP": 0, "ATTACK": 0, "DEFENSE": 0, "SPEED": 0, "SPECIAL": 0}, lvl=5,
-                 needs_stone=False):
+                 lvl=5, needs_stone=False, ev=None, ivs=None):
         self.needs_stone = needs_stone
         self.name = name
         self.lvl = lvl
         self.base_stats = base_stats
-        self.EV = ev
-        self.IV = {stat: random.randint(0, 15) for stat in self.base_stats.keys() if stat != "HP"}
+        if ev is None:
+            self.EV = {"HP": 0, "ATTACK": 0, "DEFENSE": 0, "SPEED": 0, "SPECIAL": 0}
+        else:
+            self.EV = ev
+        if ivs is None:
+            self.IV = {stat: random.randint(0, 15) for stat in self.base_stats.keys() if stat != "HP"}
+        else:
+            self.IV = ivs
         hp_binary = ''
         for item in self.IV.values():
             hp_binary += str(bin(item)[-1])
@@ -124,6 +133,7 @@ class Pokemon:
         self.gender = gender
         self.moves = []
         self.evolutions = evolutions
+        self.ko = False
 
     # prints a description of the pokemon
     def __repr__(self):
@@ -140,10 +150,40 @@ class Pokemon:
                                                                                           self.current_hp,
                                                                                           self.moves)
 
-    # calculates EV gain of a pokemon when called (usually after a battle)
+    # defines weather or not a pokemon is passed out / finished
+    def Feinted(self):
+        if self.hp <= 0:
+            self.ko = True
+
+    # defines when a pokemon's hp changes
+    def Hp_update(self, value):
+        if value <= 0:
+            self.current_hp += value
+            # placeholder update
+            print("{0} took {1} damage".format(self.name, -value))
+            if self.current_hp <= 0:
+                self.current_hp = 0
+                print("{0} has feinted".format(self.name))
+                self.feinted
+        if value > 0:
+            self.current_hp += value
+            if self.current_hp > self.hp:
+                self.current_hp = self.hp
+                print("{0} was healed to full.".format(self.name))
+            else:
+                print("{0} gained {1} HP!".format(self.name, value))
+
+    # calculates EV gain of a pokemon when called (usually after a battle) / finished
     def Ev_gain(self, op_pokemon):
         for stat in op_pokemon.base_stats:
             self.EV[stat] += op_pokemon.base_stats[stat]
+
+    # passes data to the returned evolved pokemon, instance should be a new instance with the same attributes /
+    # unfinished
+    def Evolve_lvl(self):
+        if self.name is not str(get_pokemon(self.pokemon)):
+            evolution = self.evolutions.pop("evolution")
+            pass
 
     # changes pokemon level, unfinished/needs to check possible moves to learn
     def Level_up(self):
@@ -157,10 +197,10 @@ class Pokemon:
             self.xp_to_next = (self.lvl + 1) ** 3
             # weather or not the pokemon evolves
             if self.evolutions is not None:
-                if self.lvl == self.evolutions[0][0]:
-                    self.pokemon = self.evolutions[0][1]
+                if self.lvl >= self.evolutions.get("evolution"):
+                    self.Evolve_lvl()
 
-    # adds earned xp to pokemons xp and calls the level_up function if they earned enough to be at the next level
+    # adds earned xp to pokemon's xp and calls the level_up function if they earned enough to be at the next level
     def Xp_gain(self, xp_earned):
         self.xp += xp_earned
         if self.xp == self.xp_to_next:
@@ -169,19 +209,20 @@ class Pokemon:
 
 # basic function for randomizing gender , takes r, ratio of male to females. higher ratio means more females
 # r should always be a number between 0 and 1, if no ratio is defined then it is always 50/50
-def get_gender(r = .5):
+def get_gender(r=.5):
     g = random.randint(0, 100)
-    if g >= r*100:
+    if g >= r * 100:
         return "Male"
     else:
         return "Female"
 
 
-# new code to change the games difficulty curve, randomly generated pokemon will be at a level equal to the average level
+# new code to change the games difficulty curve,
+# randomly generated pokemon will be at a level equal to the average level
 # of the trainers current roster. default level is between 1 and 5
 def pokemon_level(team=None):
     if team is None:
-        return random.randint(1,5)
+        return random.randint(1, 5)
     else:
         level = 0
         for pokemon in team:
@@ -190,171 +231,177 @@ def pokemon_level(team=None):
         return level
 
 
-# example of defining each species of pokemon's specific traits
+# function that returns a pokemon, used internally for generating random encounters
 # defining each pokemon's base characteristics, to do: add all pokemon's learnable moves.
-class Bulbasaur(Pokemon):
-    def __init__(self, name="Bulbasaur"):
-        elements = ["GRASS", "POISON"]
-        gender = get_gender()
-        base_stats = {"HP": 45, "ATTACK": 49, "DEFENSE": 45, "SPEED": 45, "SPECIAL": 65}
-        pokemon_n = 1
+def get_pokemon(n, *kwargs):
+    class Bulbasaur(Pokemon):
+        def __init__(self, name="Bulbasaur", lvl=pokemon_level(), *args):
+            elements = ["GRASS", "POISON"]
+            pokemon_n = 1
+            gender = get_gender()
+            base_stats = {"HP": 45, "ATTACK": 49, "DEFENSE": 45, "SPEED": 45, "SPECIAL": 65}
+            evolutions = {"level": 12, "evolution": 2}
 
-        super().__init__(name, elements, pokemon_n, gender, base_stats)
-class Ivysaur(Pokemon):
+            super().__init__(name, elements, pokemon_n, gender, base_stats, evolutions, lvl, *args)
 
-    def __init__(self, name= "Ivysaur"):
-        elements = ["GRASS, POISON"],
-        pokemon_n = 2
-        gender = get_gender()
-        base_stats = {"HP":60,"ATTACK":62,"DEFENSE":63,"SPEED":60,"SPECIAL":80}
-        evolutions={"Venusaur":[32,3]}
+    class Ivysaur(Pokemon):
+        def __init__(self, name="Ivysaur", lvl=pokemon_level(), *args):
+            elements = ["GRASS, POISON"],
+            pokemon_n = 2
+            gender = get_gender()
+            base_stats = {"HP": 60, "ATTACK": 62, "DEFENSE": 63, "SPEED": 60, "SPECIAL": 80}
+            evolutions = {"level": 4, 'evolution': [32, 3]}
 
-        super().__init__(name, elements, pokemon_n, gender, base_stats)
+            super().__init__(name, elements, pokemon_n, gender, base_stats, evolutions, lvl, *args)
 
-class Venusaur(Pokemon):
-    def __init__(self, name="Venusaur"):
-        elements = ["GRASS", "POISON"],
-        pokemon_n = 3
-        gender = get_gender()
-        base_stats = {"HP":80,"ATTACK":82,"DEFENSE":83, "SPEED":80,"SPECIAL":100}
+    class Venusaur(Pokemon):
+        def __init__(self, name="Venusaur", lvl=pokemon_level(), *args):
+            elements = ["GRASS", "POISON"],
+            pokemon_n = 3
+            gender = get_gender()
+            base_stats = {"HP": 80, "ATTACK": 82, "DEFENSE": 83, "SPEED": 80, "SPECIAL": 100}
 
-        super().__init__(name, elements, pokemon_n, gender, base_stats)
+            super().__init__(name, elements, pokemon_n, gender, base_stats, lvl, *args)
 
-class Charmander(Pokemon):
-    def __init__(self, name = "Charmander"):
-        elements = ["FIRE"]
-        pokemon_n = 4
-        gender = get_gender()
-        base_stats = {"HP":39,"ATTACK":52, "DEFENSE":43,"SPEED":65,"SPECIAL":50}
-        evolutions={"Charmeleon":[16,4]})
+    class Charmander(Pokemon):
+        def __init__(self, name="Charmander", lvl=pokemon_level(), *args):
+            elements = ["FIRE"]
+            pokemon_n = 4
+            gender = get_gender()
+            base_stats = {"HP": 39, "ATTACK": 52, "DEFENSE": 43, "SPEED": 65, "SPECIAL": 50}
+            evolutions = {"level": 16, "evolution": 5}
 
-        super().__init__(name, elements, pokemon_n, gender, base_stats, evolutions)
+            super().__init__(name, elements, pokemon_n, gender, base_stats, evolutions, lvl, *args)
 
-class Charmeleon(Pokemon):
-    def __init__(self, name = "Charmeleon"):
-        elements =  ["FIRE"]
-        pokemon_n = 5
-        gender = get_gender()
-        base_stats = {"HP":58,"ATTACK":64,"DEFENSE":58,"SPEED":80,"SPECIAL":65}
+    class Charmeleon(Pokemon):
+        def __init__(self, name="Charmeleon", lvl=pokemon_level(), *args):
+            elements = ["FIRE"]
+            pokemon_n = 5
+            gender = get_gender()
+            base_stats = {"HP": 58, "ATTACK": 64, "DEFENSE": 58, "SPEED": 80, "SPECIAL": 65}
+            evolutions = {"level": 36, "evolution": 6}
 
-        super().__init__(name, elements, pokemon_n, gender, base_stats)
+            super().__init__(name, elements, pokemon_n, gender, base_stats, evolutions, lvl, *args)
 
-class Charizard(Pokemon):
-    def __init__(self, name = "Charizard"):
-        elements = ["FIRE"]
-        pokemon_n = 6
-        gender = get_gender()
-        base_stats = {"HP": 78,"ATTACK": 84,"DEFENSE": 78, "SPEED": 100, "SPECIAL": 86}
+    class Charizard(Pokemon):
+        def __init__(self, name="Charizard", lvl=pokemon_level(), *args):
+            elements = ["FIRE"]
+            pokemon_n = 6
+            gender = get_gender()
+            base_stats = {"HP": 78, "ATTACK": 84, "DEFENSE": 78, "SPEED": 100, "SPECIAL": 86}
 
-        super().__init__(name, elements, pokemon_n, gender, base_stats)
+            super().__init__(name, elements, pokemon_n, gender, base_stats, lvl, *args)
 
-class Squirtle(Pokemon):
-    def __init__(self, name = "Squirtle"):
-        elements = ["WATER"]
-        pokemon_n = 7
-        gender = get_gender()
-        base_stats = {"HP": ,"ATTACK": ,"DEFENSE": , "SPEED": , "SPECIAL":}
+    """class Squirtle(Pokemon):
+        def __init__(self, name = "Squirtle", lvl = pokemon_level(), *args):
+            elements = ["WATER"]
+            pokemon_n = 7
+            gender = get_gender()
+            base_stats = {"HP": ,"ATTACK": ,"DEFENSE": , "SPEED": , "SPECIAL":}
 
-        super().__init__(name, elements, pokemon_n, gender, base_stats)
-# empty pokemon creation class
-"""class (Pokemon):
-    def __init__(self, name = ""):
-        elements = []
-        pokemon_n =
-        gender = get_gender()
-        base_stats = {"HP": ,"ATTACK": ,"DEFENSE": , "SPEED": , "SPECIAL"}
+            super().__init__(name, elements, pokemon_n, gender, base_stats, lvl *args)"""
+    # empty pokemon creation class
+    """class (Pokemon):
+        def __init__(self, name = "", lvl=pokemon_level() *args):
+            elements = []
+            pokemon_n =
+            gender = get_gender()
+            base_stats = {"HP": ,"ATTACK": ,"DEFENSE": , "SPEED": , "SPECIAL"}
+            evolutions = {"level": , "evolution": }
 
-        super().__init__(name, elements, pokemon_n, gender, base_stats)
-"""
-    #  i did this for some reason, and i forgot, could be useful later
-"""pokemon = {
-        0:None,
-        1: bulbasaur, 2: ivysaur, 3: venusaur,
-        4: charmander, 5: charmeleon, 6:charizard,
-        7: squirtle, 8: wartortle, 9: blastoise,
-        10: caterpie,11:metapod, 12:butterfree,
-        13: weedle, 14: kakuna, 15:beedrill,
-        16: pidgey, 17: pidgeotto, 18: pidgeot,
-        19: rattata, 20: raticate,
-        21: spearow, 22: fearow,
-        23: ekans, 24: arbok,
-        25: pikachu, 26: raichu,
-        27: sandshrew, 28: sandslash,
-        29: nidoran♀, 30: nidorina, 31: nidoqueen,
-        32: nidoran♂, 33: nidorino, 34: nidoking,
-        35: clefairy, 36: clefable ,
-        37: vulpix, 38: ninetales,
-        39: jigglypuff ,40: wigglytuff,
-        41: zubat, 42: golbat,
-        43: oddish, 44: gloom ,45: vileplume,
-        46: paras, 47: parasect ,
-        48: venonat, 49: venomoth,
-        50: diglett, 51: dugtrio,
-        52: meowth, 53: persian,
-        54: psyduck, 55: golduck,
-        56: mankey, 57: primeape,
-        58: growlithe, 59: arcanine,
-        60: poliwag, 61: poliwhirl, 62: poliwrath,
-        63: abra, 64: kedabra, 65: alakazam,
-        66: machop, 67: machoke, 68: machamp,
-        69: bellsprout, 70: weepinbell, 71: victreebel,
-        72: tentacool, 73: tentacruel,
-        74: geodude, 75: graveler, 76: golem,
-        77: ponyta, 78: rapidash ,
-        79: slowpoke, 80: slowbro,
-        81: magnemite, 82:magneton,
-        83: farfetchd,
-        84: doduo, 85: dodrio,
-        86: seel, 87: dewgong,
-        88: grimer, 89: muk,
-        90: shellder, 91: cloyster,
-        92: gastly, 93: haunter, 94: gengar,
-        95: onyx,
-        96: drowzee, 97: hypno,
-        98: crabby, 99: kingler,
-        100: voltorb, 101: electrode,
-        102: exeggcute, 103: exeggutor,
-        104: cubone, 105: marowak,
-        106: hitmonlee, 107: hitmonchan,
-        108: lickitung,
-        109: koffing, 110: weezing,
-        111: ryhorn,
-        112: rhydon,
-        113: chansey,
-        114: tangela,
-        115: kangaskhan,
-        116: horsea, 117: seadra,
-        118: goldeen, 119: seaking,
-        120: staryu, 121: starmie,
-        122: Mr_Mime,
-        123: Scyther,
-        124: jynx,
-        125: electabuzz,
-        126: magmar,
-        127: pinsir,
-        128: tauros,
-        129: magikarp, 130: gyarados,
-        131: lapras,
-        132: ditto,
-        133: eevee, 134: vaporeon, 135: jolteon, 136: flareon,
-        137: porygon,
-        138: omanyte, 139: omastar,
-        140: kabuto, 141: kabutops,
-        142: aerodactyl,
-        143: snorlax,
-        144: articuno,
-        145: zapdos,
-        146: moltres,
-        147: dratini, 148: dragonair, 149: dragonite,
-        150: mewtwo,
-        151: mew}"""
+            super().__init__(name, elements, pokemon_n, gender, base_stats, evolutions, lvl, *args)
+    """
+    #  List of pokemon, used to return the class for the current pokemon
+    pokemon = {
+        0: None,
+        1: Bulbasaur(*kwargs), 2: Ivysaur(*kwargs), 3: Venusaur(*kwargs),
+        4: Charmander(*kwargs), 5: Charmeleon(*kwargs), 6: Charizard(*kwargs),
+        # 7: Squirtle(*args), 8: Wartortle(*args), 9: Blastoise(*args),
+        # 10: caterpie,11:metapod, 12:butterfree,
+        # 13: weedle, 14: kakuna, 15:beedrill,
+        # 16: pidgey, 17: pidgeotto, 18: pidgeot,
+        # 19: rattata, 20: raticate,
+        # 21: spearow, 22: fearow,
+        # 23: ekans, 24: arbok,
+        # 25: pikachu, 26: raichu,
+        # 27: sandshrew, 28: sandslash,
+        # 29: nidoran♀, 30: nidorina, 31: nidoqueen,
+        # 32: nidoran♂, 33: nidorino, 34: nidoking,
+        # 35: clefairy, 36: clefable ,
+        # 37: vulpix, 38: ninetales,
+        # 39: jigglypuff ,40: wigglytuff,
+        # 41: zubat, 42: golbat,
+        # 43: oddish, 44: gloom ,45: vileplume,
+        # 46: paras, 47: parasect ,
+        # 48: venonat, 49: venomoth,
+        # 50: diglett, 51: dugtrio,
+        # 52: meowth, 53: persian,
+        # 54: psyduck, 55: golduck,
+        # 56: mankey, 57: primeape,
+        # 58: growlithe, 59: arcanine,
+        # 60: poliwag, 61: poliwhirl, 62: poliwrath,
+        # 63: abra, 64: kedabra, 65: alakazam,
+        # 66: machop, 67: machoke, 68: machamp,
+        # 69: bellsprout, 70: weepinbell, 71: victreebel,
+        # 72: tentacool, 73: tentacruel,
+        # 74: geodude, 75: graveler, 76: golem,
+        # 77: ponyta, 78: rapidash ,
+        # 79: slowpoke, 80: slowbro,
+        # 81: magnemite, 82:magneton,
+        # 83: farfetchd,
+        # 84: doduo, 85: dodrio,
+        # 86: seel, 87: dewgong,
+        # 88: grimer, 89: muk,
+        # 90: shellder, 91: cloyster,
+        # 92: gastly, 93: haunter, 94: gengar,
+        # 95: onyx,
+        # 96: drowzee, 97: hypno,
+        # 98: crabby, 99: kingler,
+        # 100: voltorb, 101: electrode,
+        # 102: exeggcute, 103: exeggutor,
+        # 104: cubone, 105: marowak,
+        # 106: hitmonlee, 107: hitmonchan,
+        # 108: lickitung,
+        # 109: koffing, 110: weezing,
+        # 111: ryhorn,
+        # 112: rhydon,
+        # 113: chansey,
+        # 114: tangela,
+        # 115: kangaskhan,
+        # 116: horsea, 117: seadra,
+        # 118: goldeen, 119: seaking,
+        # 120: staryu, 121: starmie,
+        # 122: Mr_Mime,
+        # 123: Scyther,
+        # 124: jynx,
+        # 125: electabuzz,
+        # 126: magmar,
+        # 127: pinsir,
+        # 128: tauros,
+        # 129: magikarp, 130: gyarados,
+        # 131: lapras,
+        # 132: ditto,
+        # 133: eevee, 134: vaporeon, 135: jolteon, 136: flareon,
+        # 137: porygon,
+        # 138: omanyte, 139: omastar,
+        # 140: kabuto, 141: kabutops,
+        # 142: aerodactyl,
+        # 143: snorlax,
+        # 144: articuno,
+        # 145: zapdos,
+        # 146: moltres,
+        # 147: dratini, 148: dragonair, 149: dragonite,
+        # 150: mewtwo,
+        # 151: mew
+    }
+    if n in pokemon:
+        return pokemon[n]
 
 
-def get_pokemon_name(n):
-    name = str(pokemon[n])
-
-
-bulbasaur = Bulbasaur()
-bulbasaur2 = Bulbasaur()
+bulbasaur = get_pokemon(1, "Bulby", 10)
+bulbasaur.Ev_gain(bulbasaur)
+bulbasaur.Ev_gain(bulbasaur)
+bulbasaur.Ev_gain(bulbasaur)
+print(bulbasaur.EV)
 print(bulbasaur)
-print(bulbasaur2)
+print(bulbasaur)
